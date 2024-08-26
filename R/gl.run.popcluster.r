52f9ba5c929d832fa02b52d9010f0f3f5af9844f
Ching Ching Lau <chingchinglau@Chings-MacBook-Air.local>
2024-08-26 14:07:24 +1000

    set default value and change path of output

--- R/gl.run.popcluster.r
+++ R/gl.run.popcluster.r
@@ -21,19 +21,17 @@
 #' @param minK Minimum K
 #' @param maxK Maximum K 
 #' @param rep Number of replicates runs per K
-#' @param search_relate Search using assignment_prob/relatedness: 0=N, 1=Y 
-#' @param allele_freq Output allele frequency: 0=N, 1=Y
+#' @param search_relate Search using assignment_prob/relatedness: 0=N, 1=Y [default 0]
+#' @param allele_freq Output allele frequency: 0=N, 1=Y [default 1]
 #' @param PopData PopData available: 0=N, 1=Y
-#' @param PopFlag PopFlag available: 0=N, 1=Y
-#' @param model 1=Mixture, 2=Admixture, 3=Hybridyzation, 4=Migration model
+#' @param PopFlag PopFlag available: 0=N, 1=Y. [default 0]
+#' @param model 1=Mixture, 2=Admixture, 3=Hybridyzation, 4=Migration model [default 2]
 #' @param location Individual location data available: 0=N, 1=Y 
-#' @param loc_admixture Infer locus admixture: 0=N, 1=Y 
-#' @param relatedness Compute relatedness = 0=No, 1=Wang, 2=LynchRitland
-#' @param kinship Estimate kinship: 0=N, 1=Y 
-#' @param pr_allele_freq 0=Undefined, 1=equal, 2=unequal prior allele freq
-#' @param K number of K to be saved and plotted
-#' @return The reduced genlight object, if parentals are provided; output of
-#'  PopCluster is saved to the working directory.
+#' @param loc_admixture Infer locus admixture: 0=N, 1=Y [default 1]
+#' @param relatedness Compute relatedness = 0=No, 1=Wang, 2=LynchRitland [default 0]
+#' @param kinship Estimate kinship: 0=N, 1=Y [default 0]
+#' @param pr_allele_freq 0=Undefined, 1=equal, 2=unequal prior allele freq [default 2]
+#' @return The results of PopCluster
 #' @param verbose Verbosity: 0, silent or fatal errors; 1, begin and end; 2,
 #' progress log; 3, progress and results summary; 5, full report
 #' [default 2 or as specified using gl.set.verbosity].
@@ -45,11 +43,11 @@
 #'  \url{https://groups.google.com/d/forum/dartr}
 #' @examples
 #' \dontrun{
-#' m <- gl.run.popcluster(testset.gl, popcluster.path,
+#' m <- gl.run.popcluster(testset.gl, popcluster.path,output.path,
 #'   filename, minK, maxK, 
 #'   rep, search_relate, allele_freq,PopData,PopFlag,
 #'   model, location, loc_admixture, relatedness,
-#'   kinship, pr_allele_freq, K, cleanup=TRUE, verbose=NULL
+#'   kinship, pr_allele_freq, cleanup=TRUE, verbose=NULL
 #' )
 #' }
 #' Wrapper function to run PopCluster
@@ -57,10 +55,10 @@
 #' @export 
 
 
-gl.run.popcluster <- function(gl, popcluster.path, filename, minK, maxK, 
-                              rep, search_relate, allele_freq,PopData,PopFlag,
-                              model, location, loc_admixture, relatedness, 
-                              kinship, pr_allele_freq, K, cleanup=TRUE, 
+gl.run.popcluster <- function(gl, popcluster.path, output.path, filename, minK, maxK, 
+                              rep, search_relate=0, allele_freq=1,PopData, PopFlag=0,
+                              model=2, location, loc_admixture, relatedness=0, 
+                              kinship=0, pr_allele_freq=2, cleanup=TRUE, 
                              # plot.display=TRUE,
                              # plot.out = TRUE,
                              # plot_theme = theme_dartR(),
@@ -104,33 +102,43 @@ gl.run.popcluster <- function(gl, popcluster.path, filename, minK, maxK,
   rownames(genotype) <- NULL
   if (location==1){
     lat <- gl@other$latlon$lat
-    lon <- gl@other$latlon$lon } else if (location==0) {
+    lon <- gl@other$latlon$lon 
+    names <- data.frame(id = paste0(sample_name, " ",family, " ",PopFlag, " ", lat, " ", lon))} else if (location==0) {
       lat <- NULL
       lon <- NULL
+      names <- data.frame(id = paste0(sample_name, " ",family, " ",PopFlag))
     }
-  names <- data.frame(id = paste0(sample_name, " ",family, " ", lat, " ", lon))
   names2 <- apply(names, 1, paste0, collapse = " ")
   genotype2 <- apply(genotype, 1, paste0, collapse = "")
  
    writeLines(capture.output(
     for (i in 1:nInd(gl)){
     cat(names2[i],genotype2[i], sep = "\n")}),
-    con=paste0(popcluster.path, filename,".popcluster.dat"))
+    con=paste0(output.path, filename,".popcluster.dat"))
   
   # parameter from user input
-  parameter <- c(nInd(platypus.gl), nLoc(platypus.gl),1, 3, 333,paste0(filename,".popcluster.dat") , 
+  parameter <- c(nInd(platypus.gl), nLoc(platypus.gl),1, 0, 333,paste0(filename,".popcluster.dat") , 
                  paste0(filename,".popcluster"), 
                  0, minK, maxK, 
                  rep, search_relate, allele_freq, PopData, PopFlag, 
                  model, 1, 1, location, 1, loc_admixture, relatedness, kinship, pr_allele_freq)
   
   ## default parameter name
-  parameter_name <- c("Integer, #Individuals", "Integer, #Loci", "Boolean, All loci SNP (1/0=Y/N)", 
-                      "String, Missing allele", "Integer, Random number seed", "String, Genotypefilename", 
-                      "String, Outputfilename", "integer, 3/2/1/0 = strong/medium/weak/no scaling",                
-                      "Integer, Minimum K", "Integer, Maximum K", "Integer, Num replicate runs per K", 
-                      "Integer, 0/1=Search using assignment_prob/relatedness", "Boolean, 1/0=Output allele frequency:YES/NO",                     
-                      "Boolean, 1/0=PopData available:YES/NO","Boolean, 1/0=PopFlag available:YES/NO",                           
+  parameter_name <- c("Integer, #Individuals", 
+                      "Integer, #Loci", 
+                      "Boolean, All loci SNP (1/0=Y/N)", 
+                      "String, Missing allele", 
+                      "Integer, Random number seed", 
+                      "String, Genotypefilename", 
+                      "String, Outputfilename", 
+                      "integer, 3/2/1/0 = strong/medium/weak/no scaling",                
+                      "Integer, Minimum K", 
+                      "Integer, Maximum K", 
+                      "Integer, Num replicate runs per K", 
+                      "Integer, 0/1=Search using assignment_prob/relatedness", 
+                      "Boolean, 1/0=Output allele frequency:YES/NO",                     
+                      "Boolean, 1/0=PopData available:YES/NO",
+                      "Boolean, 1/0=PopFlag available:YES/NO",                           
                       "Integer, 1/2/3/4=Mixture/Admixture/Hybridyzation/Migration model", 
                       "Boolean, 1/0=Estimate locus-specific F-Statistics=Y/N",           
                       "Boolean, 1/0=Use K-Means clustering method=Y/N", 
@@ -143,17 +151,22 @@ gl.run.popcluster <- function(gl, popcluster.path, filename, minK, maxK,
   
   
   #create parameter file
-  create_parameter<-file(paste0(popcluster.path, "/", filename,".popcluster",".PcPjt"))
+  create_parameter<-file(paste0(output.path, filename,".popcluster",".PcPjt"))
   write.table(cbind(pillar::align(parameter,align="left"), paste0("!",parameter_name)), 
-              paste0(popcluster.path, "/", filename,".popcluster",".PcPjt"),sep=" ",
+              paste0(output.path, filename,".popcluster",".PcPjt"),sep=" ",
               quote=F, col.names = F, row.names = F)
   close(create_parameter)
   
   # check file existence
-  progs <- c(popcluster_version, paste0(filename,".popcluster.PcPjt"), paste0(filename,".popcluster.dat"))
-  fex <- file.exists(file.path(popcluster.path, progs))
-  if (all(fex)) {
-    file.copy(file.path(popcluster.path, progs),
+  input_file <- c(paste0(filename,".popcluster.PcPjt"), paste0(filename,".popcluster.dat"))
+  fex <- file.exists(file.path(popcluster.path, popcluster_version))
+  fex2 <- file.exists(file.path(output.path, input_file))
+  
+  if (all(fex) & all(fex2)) {
+    file.copy(file.path(popcluster.path, popcluster_version),
+              to = tempd,
+              overwrite = TRUE, recursive = TRUE)
+    file.copy(file.path(output.path, input_file),
               to = tempd,
               overwrite = TRUE, recursive = TRUE)
   } else{
@@ -185,8 +198,9 @@ gl.run.popcluster <- function(gl, popcluster.path, filename, minK, maxK,
   #res <- read.delim(file.path(tempd,paste0(filename,".popcluster_K_", maxK, "_R_", rep)))
   
   # Select file to save and plot later
-  file.copy(file.path(tempd, paste0(filename,".popcluster_K_", maxK, "_R_", rep)),
-            to = popcluster.path,
+  list_of_files <- list.files(tempd, filename) 
+  file.copy(file.path(tempd, list_of_files),
+            to = output.path,
             overwrite = F, recursive = TRUE)
   
   setwd(old.path)
