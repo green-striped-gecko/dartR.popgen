7279fabfd8e808b2b1b4c6d15dede885f681f844
Ching Ching Lau <chingchinglau@Chings-MacBook-Air.local>
2024-09-16 16:07:25 +1000

    add Order and Pop to Q matrices

--- R/gl.run.snmf.r
+++ R/gl.run.snmf.r
@@ -92,6 +92,8 @@ gl.run.snmf <- function(x=NULL, filename="output", minK=NULL, maxK=NULL, rep=NUL
   Q <- read.table(list.files(best_run_path[i], pattern = ".Q", full.names = T))
   colnames(Q) <- paste0("Pop_",seq(1, K_range[i]))
   Q$Label <- as.character(x$ind.names)
+  Q$Order <- 1:nInd(x)
+  Q$Pop <- as.character(x$pop)
   Q_matrices[[i]] <- Q
   }
   names(Q_matrices) <- paste0("K", K_range)
