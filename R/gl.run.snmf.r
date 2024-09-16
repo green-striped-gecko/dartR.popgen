67e66edf665d7be21d73f6229fdcab5a20edceb1
Ching Ching Lau <chingchinglau@Chings-MacBook-Air.local>
2024-09-16 16:43:54 +1000

    change Pop name in Q matrix

--- R/gl.run.snmf.r
+++ R/gl.run.snmf.r
@@ -94,7 +94,7 @@ gl.run.snmf <- function(x=NULL, filename="output", minK=NULL, maxK=NULL, rep=NUL
   Q$Label <- as.character(x$ind.names)
   Q$Order <- 1:nInd(x)
   for (j in 1:nrow(Q)){
-    Q$Pop[j] <- names(which.max(Q[j,paste0("Pop_",seq(1, K_range[i]))]))
+    Q$Pop[j] <- sub("Pop_", "", names(which.max(Q[j,paste0("Pop_",seq(1, K_range[i]))])))
   }
   Q_matrices[[i]] <- Q
   Q <- NULL
